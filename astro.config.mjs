ea8a1237d55c4aa26048732ea5237c1b3b3d73bb
gunapalani <gunapalani19@outlook.com>
2024-03-10 12:33:30 +0530

    fix: feat initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+});
