8c3ddeb95aae1ecefd62610e4f6154f4312df92e
gunapalani <gunapalani19@outlook.com>
2024-03-17 10:57:07 +0530

    nav bar fixes and home and footer added

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,58 +1,54 @@
 let toggle = true;
 function selectElementById(className) {
-	return document.getElementById(`${className}`);
+  return document.getElementById(`${className}`);
 }
 const sections = [
-	selectElementById("homeBanner"),
-	selectElementById("insights"),
-	selectElementById("reward"),
-	selectElementById("feature"),
-	selectElementById("purpose"),
-	selectElementById("testimonial"),
-	selectElementById("faq"),
-	selectElementById("blogs"),
+  selectElementById("homeBanner"),
+  selectElementById("problems"),
+  selectElementById("solutions"),
+  selectElementById("god"),
+  selectElementById("destination"),
+  selectElementById("prayer"),
 ];
 
 const navItems = {
-	homeBanner: selectElementById("home-nav"),
-	insights: selectElementById("insights-nav"),
-	reward: selectElementById("reward-nav"),
-	feature: selectElementById("feature-nav"),
-	purpose: selectElementById("purpose-nav"),
-	testimonial: selectElementById("testimonial-nav"),
-	faq: selectElementById("faq-nav"),
-	blogs: selectElementById("blogs-nav"),
+  homeBanner: selectElementById("home-nav"),
+  problems: selectElementById("problems-nav"),
+  solutions: selectElementById("solutions-nav"),
+  god: selectElementById("god-nav"),
+  destination: selectElementById("destination-nav"),
+  prayer: selectElementById("prayer-nav"),
 };
 
 function observerCallback(entries) {
-	entries.forEach((entry) => {
-		if (entry.isIntersecting) {
-			const navItem = navItems[entry.target.id];
-			navItem.classList.add("active");
-			Object.values(navItems).forEach((item) => {
-				if (item !== navItem) {
-					item.classList.remove("active");
-				}
-			});
-		}
-	});
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const navItem = navItems[entry.target.id];
+      navItem.classList.add("active");
+      Object.values(navItems).forEach((item) => {
+        if (item !== navItem) {
+          item.classList.remove("active");
+        }
+      });
+    }
+  });
 }
 
 const observer = new IntersectionObserver(observerCallback);
 try {
-	sections.forEach((sec) => {
-		observer.observe(sec);
-	});
+  sections.forEach((sec) => {
+    observer.observe(sec);
+  });
 } catch (e) {}
 function toggleMobileMenu() {
-	const mobileMenu = document.getElementById("hamburger");
-	if (toggle) {
-		mobileMenu.classList.add("flex");
-		mobileMenu.classList.remove("hidden");
-		toggle = false;
-	} else {
-		mobileMenu.classList.add("hidden");
-		mobileMenu.classList.remove("flex");
-		toggle = true;
-	}
+  const mobileMenu = document.getElementById("hamburger");
+  if (toggle) {
+    mobileMenu.classList.add("flex");
+    mobileMenu.classList.remove("hidden");
+    toggle = false;
+  } else {
+    mobileMenu.classList.add("hidden");
+    mobileMenu.classList.remove("flex");
+    toggle = true;
+  }
 }
