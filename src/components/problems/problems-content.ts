39eac48ed70163900dfb9624b7f8fc30d455cc87
gunapalani <gunapalani19@outlook.com>
2024-03-17 14:36:27 +0530

    feat: all featurs done

---
+++ src/components/problems/problems-content.ts
@@ -0,0 +1,38 @@
+export const problems: any = [
+  {
+    title: "Fallen World",
+    content:
+      " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum soluta quia ea architecto qui. Sunt, id ut culpa iusto numquam, neque consequatur porrodolorum, placeat nisi velit repellat natus odio!",
+    imgUrl: "/assets/problems/fallen-world.png",
+  },
+  {
+    title: "Wrong Decisions",
+    content:
+      " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum soluta quia ea architecto qui. Sunt, id ut culpa iusto numquam, neque consequatur porrodolorum, placeat nisi velit repellat natus odio!",
+    imgUrl: "/assets/problems/wrong-dec.png",
+  },
+  {
+    title: "Wrong Priorities",
+    content:
+      " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum soluta quia ea architecto qui. Sunt, id ut culpa iusto numquam, neque consequatur porrodolorum, placeat nisi velit repellat natus odio!",
+    imgUrl: "/assets/problems/wrong-prior.png",
+  },
+  {
+    title: "Wrong Choice",
+    content:
+      " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum soluta quia ea architecto qui. Sunt, id ut culpa iusto numquam, neque consequatur porrodolorum, placeat nisi velit repellat natus odio!",
+    imgUrl: "/assets/problems/wrong-choice.png",
+  },
+  {
+    title: "Natural Causes",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum soluta quia ea architecto qui. Sunt, id ut culpa iusto numquam, neque consequatur porrodolorum, placeat nisi velit repellat natus odio!",
+    imgUrl: "/assets/problems/natural-cause.png",
+  },
+  {
+    title: "Choosing the Master",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum soluta quia ea architecto qui. Sunt, id ut culpa iusto numquam, neque consequatur porrodolorum, placeat nisi velit repellat natus odio!",
+    imgUrl: "/assets/problems/choosing-mast.png",
+  },
+];
