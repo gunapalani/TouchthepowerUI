343d1258955fc8282d25af2512d75da6762cb36c
gunapalani <gunapalani19@outlook.com>
2024-03-23 18:49:41 +0530

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
-import netlify from "@astrojs/netlify";
 export default defineConfig({
   output: "server",
-  adapter: netlify({
-    edgeMiddleware: true,
-  }),
   integrations: [react(), tailwind()],
+  adapter: node({
+    mode: "standalone",
+  }),
   server: {
     port: 3000,
     host: "0.0.0.0",
