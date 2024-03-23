e4e3afc2d7593007486519b8bb68fddf38e1e123
gunapalani <gunapalani19@outlook.com>
2024-03-23 17:33:55 +0530

    Commit made from VS Code Azure Static Web Apps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 export default defineConfig({
   output: "server",
-  integrations: [react(), tailwind()],
-  adapter: node({
-    mode: "standalone",
+  adapter: netlify({
+    edgeMiddleware: true,
   }),
+  integrations: [react(), tailwind()],
   server: {
     port: 3000,
     host: "0.0.0.0",
